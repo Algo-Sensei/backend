7973ce82b1684df12de7cacd51a7a49f2a133b7d
jamardines <ardines.main.gg@gmail.com>
2026-02-06 11:59:45 +0800

    first commit

---
+++ src/main/java/com/algosensei/backend/AlgosenseiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.algosensei.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgosenseiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgosenseiBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/algosensei/backend/AlgosenseiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algosensei.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgosenseiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
